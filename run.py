9db15cecf138e6afb2c13359185134b05f8a1bd2
Sammy92dec Tekheste <samrawit.samuel.tk@gmail.com>
2024-06-09 23:58:07 +0000

    Add: the underscore and mulitply by seven so it will be the same number as the column and row.

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 print("===== BATTLESHIP Board =====")
-print("   0  1  2  3  4  5  6 ")
+print("    0  1  2  3  4  5  6 ")
 
 for x in range(7):
     row = ""
-    print(x, "", row)
+    print(x, " ", "_  " * 7)
