7fba98386238e40b9a7f55884f94a90f00dc97e7
Sammy92dec Tekheste <samrawit.samuel.tk@gmail.com>
2024-06-10 00:43:29 +0000

    Add:Welcome text and instrutions.Add:while statement to re-enter name  for invalid names if player inputs non alphabetic character

--- run.py
+++ run.py
@@ -1,6 +1,36 @@
-print("======= BATTLESHIP Board =======")
-print("    0  1  2  3  4  5  6  7  8  9")
+import random
 
-for x in range(10):
-    row = ""
-    print(x, " ", "_  " * 10)
+def is_valid_name(name):
+    return name.isalpha()
+
+print("~~~~ Welcome to the Battleship!! ~~~~")
+player_name = input("Enter your name: ")
+while not is_valid_name(player_name):
+    print("Invalid name.Please enter only alphabetic character.")
+    player_name = input("Enter your name: ")
+print("\n")
+print(f"READY {player_name}! Your enemy's Battleboard is ready.")
+print("\n")
+
+# Instructions on how to play the game
+
+print("You have a total of 20 turns to sink 3 hidden ships.")
+print("Guess a row and a column (0 - 9).")
+print("You will see X when you hit a ship,Otherwise O if you miss.")
+print("Type'exit' instead of row number if you want to quit")
+print("Choose Wisely,GOODLUCK!")
+print("\n")
+
+def show_board():
+    print("======= BATTLESHIP Board =======")
+    print("    0  1  2  3  4  5  6  7  8  9")
+
+    place = 0
+    for x in range(10):
+        row = ""
+        for y in range(10):          
+            ch = " * "
+            row += ch
+            place += 1
+        print(x, "", row)
+show_board()
