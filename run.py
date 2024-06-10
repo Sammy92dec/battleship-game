0fb419dd703ed50d4c264872ae7545c882b02725
Sammy92dec Tekheste <samrawit.samuel.tk@gmail.com>
2024-06-10 01:16:37 +0000

    Add hit or miss function.Exit to stop the game too

--- run.py
+++ run.py
@@ -1,5 +1,31 @@
 import random
 
+
+# Get inputs like row and column, Input to exit the game
+
+
+def get_shot(guesses):
+    while True:
+        row = input("Please enter the row (0 - 6): ")
+        if row.lower() == "exit":
+            return "exit"
+
+        try:
+            row = int(row)
+            col = int(input("Please enter the column (0 - 6): "))
+
+            shot = 7 * row + col
+
+            if shot < 0 or shot > 48:
+                print("Incorrect coordinates, Please try again")
+            elif shot in guesses:
+                print("Incorrect, it's been used already")
+            else:
+                return shot
+        except ValueError:
+            print("Incorrect entry,Please enter your number.")
+guesses = hit + miss
+
 # Invalid name
 
 
@@ -33,16 +59,27 @@ print("\n")
 # Battle board 
 
 
-def show_board():
+def show_board(hit, miss):
     print("======= BATTLESHIP Board =======")
     print("    0  1  2  3  4  5  6  7  8  9")
 
     place = 0
     for x in range(10):
         row = ""
-        for y in range(10):          
-            ch = " * "
+        for y in range(10):
+            if place in hit:
+                ch = " X "
+            elif place in miss:
+                ch = " O "
+            else:         
+                ch = " * "
             row += ch
             place += 1
         print(x, "", row)
 show_board()
+
+def play_battleship_game():
+    hit = []
+    miss = []
+    ships_found = 0
+play_battleship_game()
