8aa5f4ff515c96f36b8dbe076ccc6027cc8fa818
Sammy92dec Tekheste <samrawit.samuel.tk@gmail.com>
2024-06-10 00:01:20 +0000

    Fix: the range so it will 10 instead of 7

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-print("===== BATTLESHIP Board =====")
-print("    0  1  2  3  4  5  6 ")
+print("======= BATTLESHIP Board =======")
+print("    0  1  2  3  4  5  6  7  8  9")
 
-for x in range(7):
+for x in range(10):
     row = ""
-    print(x, " ", "_  " * 7)
+    print(x, " ", "_  " * 10)
